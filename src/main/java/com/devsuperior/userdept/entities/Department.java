d373e43402e483dee6fdd0f880796668be4389bb
William-ops-crypto <william.s.91@hotmail.com>
2022-12-23 00:18:57 -0300

    Teste

--- src/main/java/com/devsuperior/userdept/entities/Department.java
+++ src/main/java/com/devsuperior/userdept/entities/Department.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Table;
 @Table(name = "tb_department")
 public class Department {
 	
-	
+	//comentario teste
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
